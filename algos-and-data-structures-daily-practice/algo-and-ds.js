3fc522feb988fc72b10ab6e893479a5d2830c17e
Marilyn Balkaransingh <marilynbalkaransingh@marilyns-air.home>
2021-03-07 20:05:18 -0500

    alternating chatacters solved - section created

--- algos-and-data-structures-daily-practice/algo-and-ds.js
+++ algos-and-data-structures-daily-practice/algo-and-ds.js
@@ -285,3 +285,15 @@ function timeConversion(s) {
      return timeArr.join(':');
  }
  
+ //Alternating Characters - Hackerrank
+ function alternatingCharacters(s) {
+    let delLetters = 0;
+    for (let i = 0; i < s.length; i++){
+        if(s[i] === s[i+1]){
+            delLetters++;
+        }else {
+            continue;
+        }
+    }return delLetters;
+
+}
